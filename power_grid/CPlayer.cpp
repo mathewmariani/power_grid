d252bdb59f1de663103ea2d1c499fb80b51c3bb9
Mat Mariani <someone.mariani@gmail.com>
2016-02-26 21:28:09 -0500

    Initial Commit
    [CPlayer.h]

---
+++ power_grid/CPlayer.cpp
@@ -0,0 +1,79 @@
+#include "CPlayer.h"
+
+CPlayer::CPlayer(std::string name) :
+	m_sName(name),
+	m_iMoney(0),
+	m_iCoal(0),
+	m_iOil(0),
+	m_iGarbage(0),
+	m_iUranium(0) {
+
+}
+
+CPlayer::~CPlayer() {
+
+}
+
+std::string CPlayer::GetName() {
+	return m_sName;
+}
+
+int CPlayer::GetMoney() {
+	return m_iMoney;
+}
+
+int CPlayer::GetCoal() {
+	return m_iCoal;
+}
+
+int CPlayer::GetOil() {
+	return m_iOil;
+}
+
+int CPlayer::GetGarbage() {
+	return m_iGarbage;
+}
+
+int CPlayer::GetUranium() {
+	return m_iUranium;
+}
+
+const std::string CPlayer::GetName() const {
+	return m_sName;
+}
+
+const int CPlayer::GetMoney() const {
+	return m_iMoney;
+}
+
+const int CPlayer::GetCoal() const {
+	return m_iCoal;
+}
+
+const int CPlayer::GetOil() const {
+	return m_iOil;
+}
+
+const int CPlayer::GetGarbage() const {
+	return m_iGarbage;
+}
+
+const int CPlayer::GetUranium() const {
+	return m_iUranium;
+}
+
+BuyResult_e CPlayer::AttemptToBuyCoal() {
+
+}
+
+BuyResult_e CPlayer::AttemptToBuyOil() {
+
+}
+
+BuyResult_e CPlayer::AttemptToBuyGarbage() {
+
+}
+
+BuyResult_e CPlayer::AttemptToBuyUranium() {
+
+}
