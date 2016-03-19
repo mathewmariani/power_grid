f6b608fbbf141371c753f773815f43600cf29b4b
Mat Mariani <someone.mariani@gmail.com>
2016-03-19 17:31:06 -0400

    Andres added maps

---
+++ power_grid/CMapData.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CCityData.h"
+#include "ISubject.h"
+#include "base.h"
+
+class CMapData : public ISubject {
+public:
+	CMapData();
+	~CMapData();
+
+	void Serialize(pugi::xml_node &parent);
+	void AddCity(std::string name, std::string region);
+
+	CCityData GetCityByName(std::string name);
+
+private:
+	std::vector<CCityData> m_vCities;
+};
---
+++ power_grid/CMapView.cpp
@@ -0,0 +1,17 @@
+#include "CMapView.h"
+
+CMapView::CMapView() {}
+CMapView::~CMapView() {}
+
+void CMapView::Initialize(CMapData* pData) {
+	if (pData == nullptr) {
+		return;
+	}
+
+	m_pModel = pData;
+	m_pModel->Attach(this);
+}
+
+void CMapView::Update() {
+
+}
